c7c5da7b1235b51f2a6e42371a0da035880b8e66
jbortweb <jbortweb@gmail.com>
2022-07-02 16:59:50 +0200

    reparar rama git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import "./App.css";
 import Vista from "./components/Vista/Vista";
-<<<<<<< HEAD
-=======
 
-const App = () => {
-  
->>>>>>> master
 
 const App = () => {
   return (
     <div>
       <Vista/>
     </div>
-<<<<<<< HEAD
-  )
-}
-  
-export default (App);
-=======
+
     );
 }
 export default App;
->>>>>>> master
